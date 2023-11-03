9967107434cbe4e47c835b1579cb538ab3bbf02f
Vale <>
2023-11-03 22:37:52 +0100

    VERDE DIRETTO: è bastato creare la struttura della classe in modo che il primo test passasse

---
+++ src/main/java/it/unimi/di/sweng/lab02/BowlingGame.java
@@ -0,0 +1,13 @@
+package it.unimi.di.sweng.lab02;
+
+public class BowlingGame implements Bowling{
+    @Override
+    public void roll(int pins) {
+
+    }
+
+    @Override
+    public int score() {
+        return 0;
+    }
+}
